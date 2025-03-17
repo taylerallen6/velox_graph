395e29f788f8156e6e90db2981b099dbc992797a
Tayler Allen <taylerallen6@gmail.com>
2025-03-17 15:10:28 -0500

    initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,254 @@
+pub mod tests;
+
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
+use std::io;
+use std::result::Result::Err;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum VeloxGraphError {
+    #[error("Failed to create/load database file: {0}")]
+    FileFail(String),
+    #[error("io Error")]
+    IoError(#[from] io::Error),
+    #[error("toml deserialize Error")]
+    TomlDeserializeError(#[from] toml::de::Error),
+    #[error("toml serialize Error")]
+    TomlSerializeError(#[from] toml::ser::Error),
+
+    #[error("database: Empty_slots vector is empty")]
+    EmptySlotsVectorIsEmpty,
+    #[error("database: Slot {0} is not allocated. No data here. This means that slot >= nodes_vector.len(). You cannot access, update, or remove a slot that is not allocated.")]
+    SlotNotAllocated(usize),
+    #[error("database: Slot {0} is not used. No data here. Slot should already be in empty_slots vector. You cannot access, update, or remove a slot that is not in use.")]
+    SlotNotUsed(usize),
+
+    #[error("unknown database error")]
+    Unknown,
+}
+
+const SLOT_USED: bool = true;
+const SLOT_NOT_USED: bool = false;
+
+// const SETTINGS_FILE_NAME: &str = "vdb_settings.toml";
+
+// INFO: TOML serialized
+#[derive(Serialize, Deserialize, Debug)]
+pub struct VeloxGraghSettings {
+    version: String,
+}
+
+impl VeloxGraghSettings {
+    fn new() -> VeloxGraghSettings {
+        VeloxGraghSettings {
+            version: String::from("2.0"),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Connection<ConnectionT: Clone> {
+    pub node_id: usize,
+    pub data: ConnectionT,
+}
+
+#[derive(Clone)]
+pub struct Node<NodeT: Clone, ConnectionT: Clone> {
+    node_id: usize,
+    pub data: NodeT,
+
+    connections_forward: HashMap<usize, Connection<ConnectionT>>,
+    connections_backward: HashSet<usize>,
+}
+
+#[derive(Clone)]
+pub struct NodeOption<NodeT: Clone, ConnectionT: Clone> {
+    is_used: bool,
+    node: Node<NodeT, ConnectionT>,
+}
+
+impl<NodeT: Clone, ConnectionT: Clone> Node<NodeT, ConnectionT> {
+    pub fn new(node_id: usize, node_data: NodeT) -> Node<NodeT, ConnectionT> {
+        Node {
+            node_id,
+            data: node_data,
+            connections_forward: HashMap::new(),
+            connections_backward: HashSet::new(),
+        }
+    }
+
+    pub fn connections_forward_get<'a>(&'a self) -> &'a HashMap<usize, Connection<ConnectionT>> {
+        &self.connections_forward
+    }
+
+    pub fn connections_backward_get<'a>(&'a self) -> &'a HashSet<usize> {
+        &self.connections_backward
+    }
+
+    fn connection_forward_create(&mut self, node_id_value: usize, connection_data: ConnectionT) {
+        let new_connection = Connection {
+            node_id: node_id_value,
+            data: connection_data,
+        };
+
+        self.connections_forward
+            .insert(node_id_value, new_connection);
+    }
+
+    fn connection_forward_delete(&mut self, node_id_value: usize) {
+        //self.connections_forward.remove(&node_id_value);
+
+        match self.connections_forward.get(&node_id_value) {
+            Some(_connection_val) => {
+                self.connections_forward.remove(&node_id_value);
+            }
+            //None => println!("forward connection already not there."),
+            None => (),
+        };
+    }
+    fn connection_backward_create(&mut self, node_id_value: usize) {
+        self.connections_backward.insert(node_id_value);
+    }
+
+    fn connection_backward_delete(&mut self, node_id_value: usize) {
+        //self.connections_backward.remove(&node_id_value);
+
+        match self.connections_backward.get(&node_id_value) {
+            Some(_connection_val) => {
+                self.connections_backward.remove(&node_id_value);
+            }
+            //None => println!("backward connection already not there."),
+            None => (),
+        };
+    }
+}
+
+pub struct VeloxGraph<NodeT: Clone, ConnectionT: Clone> {
+    #[allow(dead_code)]
+    settings: VeloxGraghSettings,
+
+    // INFO: metedata.
+    // latest_available_slot: usize,
+    pub num_entries: usize,
+    // num_used_slots: usize,
+    nodes_vector: Vec<NodeOption<NodeT, ConnectionT>>,
+    empty_slots: Vec<usize>,
+}
+
+impl<NodeT: Clone, ConnectionT: Clone> VeloxGraph<NodeT, ConnectionT> {
+    pub fn new() -> VeloxGraph<NodeT, ConnectionT> {
+        let settings = VeloxGraghSettings::new();
+
+        VeloxGraph {
+            settings,
+            num_entries: 0,
+            nodes_vector: Vec::new(),
+            empty_slots: Vec::new(),
+        }
+    }
+
+    pub fn node_create(&mut self, node_data: NodeT) -> usize {
+        let new_node_option = NodeOption {
+            is_used: SLOT_USED,
+            node: Node::new(0, node_data),
+        };
+
+        let new_node_id = match self.empty_slots.pop() {
+            Some(new_node_id_value) => {
+                self.nodes_vector[new_node_id_value] = new_node_option;
+                new_node_id_value
+            }
+            None => {
+                self.nodes_vector.push(new_node_option);
+                self.nodes_vector.len() - 1
+            }
+        };
+
+        self.num_entries += 1;
+
+        self.nodes_vector[new_node_id].node.node_id = new_node_id;
+
+        new_node_id
+    }
+
+    pub fn node_get<'a>(
+        &'a mut self,
+        node_id: usize,
+    ) -> Result<&'a mut Node<NodeT, ConnectionT>, VeloxGraphError> {
+        if node_id >= self.nodes_vector.len() {
+            return Err(VeloxGraphError::SlotNotAllocated(node_id));
+        }
+
+        let node_option = &mut self.nodes_vector[node_id];
+
+        if node_option.is_used == SLOT_NOT_USED {
+            return Err(VeloxGraphError::SlotNotUsed(node_id));
+        }
+
+        Ok(&mut node_option.node)
+    }
+
+    pub fn node_delete(&mut self, node_id_to_delete: usize) -> Result<(), VeloxGraphError> {
+        let node_to_delete = self.node_get(node_id_to_delete)?.clone();
+
+        node_to_delete
+            .connections_backward
+            // .clone()
+            .iter()
+            .for_each(|&connection_node_id| {
+                // let node = &mut self.nodes_vector[*connection_node_id].node;
+                let node = self.node_get(connection_node_id).unwrap();
+                node.connection_forward_delete(node_id_to_delete);
+            });
+
+        node_to_delete
+            .connections_forward
+            // .clone()
+            .keys()
+            .for_each(|&connection_node_id| {
+                // let node = &mut self.nodes_vector[*connection_node_id].node;
+                let node = self.node_get(connection_node_id).unwrap();
+                node.connection_backward_delete(node_id_to_delete);
+            });
+
+        self.empty_slots.push(node_id_to_delete);
+        self.num_entries -= 1;
+        Ok(())
+    }
+
+    pub fn nodes_connection_create(
+        &mut self,
+        first_node_id: usize,
+        second_node_id: usize,
+        connection_data: ConnectionT,
+    ) -> Result<(), VeloxGraphError> {
+        // INFO: check if both nodes exist, then create connection.
+        let _second_node = self.node_get(second_node_id)?;
+        let first_node = self.node_get(first_node_id)?;
+
+        first_node.connection_forward_create(second_node_id, connection_data);
+
+        let second_node = self.node_get(second_node_id)?;
+        second_node.connection_backward_create(first_node_id);
+
+        Ok(())
+    }
+
+    pub fn nodes_connect_delete(
+        &mut self,
+        first_node_id: usize,
+        second_node_id: usize,
+    ) -> Result<(), VeloxGraphError> {
+        // INFO: check if both nodes exist, then create connection.
+        let _second_node = self.node_get(second_node_id)?;
+        let first_node = self.node_get(first_node_id)?;
+
+        first_node.connection_forward_delete(second_node_id);
+
+        let second_node = self.node_get(second_node_id)?;
+        second_node.connection_backward_delete(first_node_id);
+
+        Ok(())
+    }
+}
---
+++ src/tests.rs
@@ -0,0 +1,254 @@
+#![cfg(test)]
+
+use super::VeloxGraph;
+
+use rand::seq::SliceRandom;
+use std::time::Instant;
+use std::time::{self, Duration};
+use std::{thread, usize};
+
+#[derive(Clone, Debug)]
+struct SomeData {
+    x: u32,
+    y: u32,
+}
+
+// INFO: TEST BASIC FUNCTIONALITY.
+#[test]
+fn test_basic_functions() {
+    let mut graph: VeloxGraph<SomeData, u32> = VeloxGraph::new();
+
+    // println!("num_entries: {}", graph.num_entries);
+    assert_eq!(graph.num_entries, 0);
+    assert_eq!(graph.empty_slots, vec![]);
+
+    let node_id = graph.node_create(SomeData { x: 134, y: 351 });
+    assert_eq!(node_id, 0);
+    // println!("num_entries: {}", graph.num_entries);
+    assert_eq!(graph.num_entries, 1);
+    assert_eq!(graph.empty_slots, vec![]);
+
+    let node = graph.node_get(node_id).unwrap();
+    // println!("node data: {:?}", node.data);
+    assert_eq!(node.data.x, 134);
+    assert_eq!(node.data.y, 351);
+
+    node.data.x += 4;
+    node.data.y = 2431;
+
+    let node = graph.node_get(node_id).unwrap();
+    // println!("node data: {:?}", node.data);
+    assert_eq!(node.data.x, 138);
+    assert_eq!(node.data.y, 2431);
+
+    let node_id2 = graph.node_create(SomeData { x: 234, y: 5 });
+    assert_eq!(node_id2, 1);
+    let node_id3 = graph.node_create(SomeData { x: 63, y: 42 });
+    assert_eq!(node_id3, 2);
+    let node_id4 = graph.node_create(SomeData { x: 2, y: 51 });
+    assert_eq!(node_id4, 3);
+
+    assert_eq!(graph.num_entries, 4);
+    assert_eq!(graph.nodes_vector.len(), 4);
+    assert_eq!(graph.empty_slots.len(), 0);
+
+    let node = graph.node_get(node_id).unwrap();
+    let forwards = node.connections_forward_get();
+    // println!("forwards: {:?}", forwards);
+    assert_eq!(forwards.len(), 0);
+
+    graph.nodes_connection_create(node_id, 2, 53245).unwrap();
+    graph.nodes_connection_create(node_id, 3, 24323).unwrap();
+
+    let node = graph.node_get(node_id).unwrap();
+    let forwards = node.connections_forward_get();
+    // println!("forwards: {:?}", forwards);
+    assert_eq!(forwards.len(), 2);
+    assert_eq!(forwards.get(&2).unwrap().node_id, 2);
+    assert_eq!(forwards.get(&2).unwrap().data, 53245);
+    assert_eq!(forwards.get(&3).unwrap().node_id, 3);
+    assert_eq!(forwards.get(&3).unwrap().data, 24323);
+
+    // for connection in node.connections_forward_get().values() {
+    //     println!("forward_connection: {:?}", connection);
+    // }
+
+    let node2 = graph.node_get(2).unwrap();
+    let backwards = node2.connections_backward_get();
+    // println!("forwards: {:?}", forwards);
+    assert_eq!(backwards.len(), 1);
+    assert_eq!(backwards.get(&0).unwrap(), &0);
+
+    // for connection in node2.connections_backward_get() {
+    //     println!("backward_connection: {:?}", connection);
+    // }
+
+    let node3 = graph.node_get(3).unwrap();
+    let backwards = node3.connections_backward_get();
+    // println!("forwards: {:?}", forwards);
+    assert_eq!(backwards.len(), 1);
+    assert_eq!(backwards.get(&0).unwrap(), &0);
+
+    // for connection in node3.connections_backward_get() {
+    //     println!("backward_connection: {:?}", connection);
+    // }
+
+    graph.node_delete(2).unwrap();
+    assert_eq!(graph.num_entries, 3);
+    assert_eq!(graph.nodes_vector.len(), 4);
+    assert_eq!(graph.empty_slots.len(), 1);
+    assert_eq!(graph.empty_slots[0], 2);
+
+    let node = graph.node_get(node_id).unwrap();
+    let forwards = node.connections_forward_get();
+    // println!("forwards: {:?}", forwards);
+    assert_eq!(forwards.len(), 1);
+    assert_eq!(forwards.get(&3).unwrap().node_id, 3);
+    assert_eq!(forwards.get(&3).unwrap().data, 24323);
+
+    // for connection in node.connections_forward_get().values() {
+    //     println!("forward_connection: {:?}", connection);
+    // }
+
+    graph.node_delete(0).unwrap();
+    assert_eq!(graph.num_entries, 2);
+    assert_eq!(graph.nodes_vector.len(), 4);
+    assert_eq!(graph.empty_slots.len(), 2);
+    assert_eq!(graph.empty_slots[1], 0);
+
+    let node3 = graph.node_get(3).unwrap();
+    let backwards = node3.connections_backward_get();
+    // println!("forwards: {:?}", forwards);
+    assert_eq!(backwards.len(), 0);
+
+    let node_id5 = graph.node_create(SomeData { x: 63452, y: 846 });
+    assert_eq!(node_id5, 0);
+
+    assert_eq!(graph.num_entries, 3);
+    assert_eq!(graph.nodes_vector.len(), 4);
+    assert_eq!(graph.empty_slots.len(), 1);
+}
+
+const NUM_NODES: usize = 10_000;
+const NUM_CONNECTIONS_CREATE: usize = 10_000;
+const NUM_CONNECTIONS_PER_NODE: usize = 10;
+
+#[test]
+fn speed_test() {
+    let delay_time = time::Duration::from_millis(1000);
+
+    let mut random_nodes: Vec<usize> = (0..NUM_NODES).collect();
+    random_nodes.shuffle(&mut rand::rng());
+
+    let mut graph: VeloxGraph<u32, u32> = VeloxGraph::new();
+    thread::sleep(delay_time);
+
+    create_nodes_test(&mut graph, NUM_NODES);
+
+    thread::sleep(delay_time);
+
+    let random_nodes_to_connect: Vec<usize> = (0..NUM_NODES).collect();
+
+    create_connections_test(
+        &mut graph,
+        NUM_CONNECTIONS_CREATE,
+        random_nodes.clone(),
+        random_nodes_to_connect,
+    );
+
+    thread::sleep(delay_time);
+
+    delete_nodes_test(&mut graph, NUM_NODES);
+}
+
+fn create_nodes_test(graph: &mut VeloxGraph<u32, u32>, num_nodes: usize) {
+    let mut create_times = Vec::new();
+    let mut timestamp: Instant;
+    let mut time_elapsed: Duration;
+
+    for i in 0..num_nodes {
+        timestamp = Instant::now();
+        graph.node_create(i as u32);
+        time_elapsed = timestamp.elapsed();
+        create_times.push(time_elapsed);
+    }
+
+    // INFO: end time test and display time elapsed.
+    let mean_time = create_times.iter().sum::<Duration>() / create_times.len() as u32;
+    println!("create: mean time per db operation: {:.2?}", mean_time);
+    let max_time = create_times.iter().max().unwrap();
+    println!("create: max time per db operation: {:.2?}", max_time);
+    let min_time = create_times.iter().min().unwrap();
+    println!("create: min time per db operation: {:.2?}", min_time);
+    println!();
+}
+
+fn create_connections_test(
+    graph: &mut VeloxGraph<u32, u32>,
+    num_connections_to_create: usize,
+    random_nodes: Vec<usize>,
+    mut random_nodes_to_connect: Vec<usize>,
+) {
+    let random_nodes = &random_nodes[..num_connections_to_create];
+
+    let mut create_connections_times = Vec::new();
+    let mut timestamp: Instant;
+    let mut time_elapsed: Duration;
+
+    for (i, &entry_index) in random_nodes.iter().enumerate() {
+        random_nodes_to_connect.shuffle(&mut rand::rng());
+        let random_nodes_to_connect = &random_nodes_to_connect[..NUM_CONNECTIONS_PER_NODE];
+
+        timestamp = Instant::now();
+
+        for &second_node in random_nodes_to_connect {
+            graph
+                .nodes_connection_create(entry_index, second_node, i as u32)
+                .unwrap();
+        }
+
+        time_elapsed = timestamp.elapsed();
+        create_connections_times.push(time_elapsed);
+    }
+
+    // INFO: end time test and display time elapsed.
+    let time_per_operation =
+        create_connections_times.iter().sum::<Duration>() / create_connections_times.len() as u32;
+    println!(
+        "create_connections: time per db operation: {:.2?}",
+        time_per_operation
+    );
+    let max_time = create_connections_times.iter().max().unwrap();
+    println!(
+        "create_connections: max time per db operation: {:.2?}",
+        max_time
+    );
+    let min_time = create_connections_times.iter().min().unwrap();
+    println!(
+        "create_connections: min time per db operation: {:.2?}",
+        min_time
+    );
+    println!();
+}
+
+fn delete_nodes_test(graph: &mut VeloxGraph<u32, u32>, num_nodes: usize) {
+    let mut delete_times = Vec::new();
+    let mut timestamp: Instant;
+    let mut time_elapsed: Duration;
+
+    for i in 0..num_nodes {
+        timestamp = Instant::now();
+        graph.node_delete(i).unwrap();
+        time_elapsed = timestamp.elapsed();
+        delete_times.push(time_elapsed);
+    }
+
+    // INFO: end time test and display time elapsed.
+    let mean_time = delete_times.iter().sum::<Duration>() / delete_times.len() as u32;
+    println!("delete: mean time per db operation: {:.2?}", mean_time);
+    let max_time = delete_times.iter().max().unwrap();
+    println!("delete: max time per db operation: {:.2?}", max_time);
+    let min_time = delete_times.iter().min().unwrap();
+    println!("delete: min time per db operation: {:.2?}", min_time);
+    println!();
+}
