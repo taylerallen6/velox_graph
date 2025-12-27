912679d81a43f965fc719c9a40d70e985fab0181
Tayler Allen <taylerallen6@gmail.com>
2025-12-27 11:28:38 -0600

    Remove unnecessary docs. Update version number.

--- doc/crates.js
+++
@@ -1,2 +0,0 @@
-window.ALL_CRATES = ["velox_graph"];
-//{"start":21,"fragment_lengths":[13]}
--- doc/trait.impl/core/fmt/trait.Display.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["velox_graph",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"velox_graph/enum.VeloxGraphError.html\" title=\"enum velox_graph::VeloxGraphError\">VeloxGraphError</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[295]}
